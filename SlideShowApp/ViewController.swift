25a2811a92df00ace67a12d4f1c2885763feb6f3
Jumpei.Iwauchi <kiyokibarock0107@gmail.com>
2020-10-28 20:00:53 +0900

    Initial Commit

---
+++ SlideShowApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SlideShowApp
+//
+//  Created by 岩内順平 on 2020/10/28.
+//  Copyright © 2020 Jumpei Iwauchi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
