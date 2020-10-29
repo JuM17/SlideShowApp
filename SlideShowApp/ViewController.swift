0b092fe692f0ebf604a70122e8d28a911193958b
Jumpei.Iwauchi <kiyokibarock0107@gmail.com>
2020-10-29 20:21:59 +0900

    よろしくお願いします

--- SlideShowApp/ViewController.swift
+++ SlideShowApp/ViewController.swift
@@ -9,12 +9,104 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var imageView : UIImageView!
+    
 
+    @IBOutlet weak var button: UIButton!
+    
+    @IBOutlet weak var button2: UIButton!
+    
+    
+    var imageIndex = 0
+    
+    var timer: Timer!
+    
+    var timer_sec: Float = 0
+    
+    
+    
+    let images = [UIImage(named: "inu1"), UIImage(named: "inu2"), UIImage(named: "inu3")]
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+      
     }
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func startButton(_ sender: Any) {
+        
+        
+        if imageIndex == 2 {
+            imageIndex = 0
+        } else {
+            imageIndex += 1
+        }
+        imageView.image = images[imageIndex]
+    }
+    @IBAction func backButton(_ sender: Any) {
+        
+        
+        
+        if imageIndex == 0 {
+            imageIndex = 2
+        } else {
+            imageIndex -= 1
+        }
+        imageView.image = images[imageIndex]
+        
+        
+    }
+    
+    
 
-
+    
+    @IBAction func startStop(_ sender: Any) {
+        
+        if timer == nil {
+            self.timer = Timer.scheduledTimer(timeInterval: 2.0, target: self, selector: #selector(onTimer(_:)), userInfo: nil, repeats: true)
+            
+        } else {
+            if self.timer != nil {
+                self.timer.invalidate()
+                self.timer = nil
+                button.isEnabled = true
+                button2.isEnabled = true
+            }
+            
+        }
+        
+        
+    }
+    @objc func onTimer(_ timer: Timer) {
+        
+        if imageIndex == 2 {
+            imageIndex = 0
+        } else {
+            imageIndex += 1
+        }
+        imageView.image = images[imageIndex]
+        button.isEnabled = false
+        button2.isEnabled = false
+    }
+    
+    
+    
+    
+    
+    
+    @IBAction func unwind(_ segue: UIStoryboardSegue) {
+    }
+    
+    @IBAction func tapView(_ sender: Any) {
+    }
+    
 }
 
